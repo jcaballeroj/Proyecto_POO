93ac4eee1cb7ec50406e9c349eb64789f29478b4
Jose Caballero <jcaballeroj@66gmail.com>
2024-04-20 23:00:38 -0500

    Subiendo archivos del Proyecto, login e interfaz

---
+++ src/main/java/com/mycompany/proyecto_poo/Proyecto_POO.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.proyecto_poo;
+
+/**
+ *
+ * @author jcaba
+ */
+public class Proyecto_POO {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
